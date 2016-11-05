6d6616fdb6bb14e52bdfddbcc114ed960b086c6a
jm672 <jm672@njit.edu>
2016-11-05 13:08:46 -0400

    Update hacknjit2016.py

--- hacknjit2016.py
+++ hacknjit2016.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 from time import sleep
 
@@ -10,4 +11,22 @@ print("Game over!")
 
 openPlz = (wordbank.txt,"r")
 readPlz = openPlz.read()
-#fdsfdsa
+
+wordBank = readPlz.split()
+
+#JJ
+stat = true
+strike = 0
+score = 0
+print("Type the word then press enter!")
+while stat == true:
+    wordQuiz = wordBank[random.randint(0,len(wordBank))]
+    wordType = input(wordQuiz)
+    if wordType == wordQuiz:
+        score += 1
+        continue
+    else:
+        strike += 1
+    if strike == 3:
+        print('Game Over!')
+        break
