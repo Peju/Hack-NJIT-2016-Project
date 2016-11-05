65d3fd3bb374be054bc140aef256656682d9c3a7
jm672 <jm672@njit.edu>
2016-11-05 14:34:38 -0400

    Edit

--- hacknjit2016.py
+++ hacknjit2016.py
@@ -2,27 +2,24 @@ import time
 import random
 
 
-openPlz = (wordbank.txt,"r")
+openPlz = ('wordbank.txt',"r")
 readPlz = openPlz.read()
 
 wordBank = readPlz.split()
 
 #JJ
-stat = true
 strike = 0
 score = 0
 print("Type the word then press enter!")
-while stat == true:
+while time.time()-start < 7:
     start = time.time()
-    while time.time()-start < 7:
-        wordQuiz = wordBank[random.randint(0,len(wordBank))]
-        wordType = input(wordQuiz)
-        if wordType == wordQuiz:
-            score += 1
-            continue
-        else:
-            strike += 1
+    wordQuiz = wordBank[random.randint(0,len(wordBank))]
+    wordType = input('Type the word '+ wordQuiz)
+    if wordType == wordQuiz:
+        score += 1
+        continue
     strike += 1
     if strike == 3:
-            print('Game Over!')
-            break
+        print('Game Over!')
+        print('Your Score: ' + str(score))
+        break
