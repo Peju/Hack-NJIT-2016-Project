6c6288d09c52918e7224a53abca70986761f6a90
Peju <pjough@gmail.com>
2016-11-05 12:39:59 -0400

    v2

--- hacknjit2016.py
+++ hacknjit2016.py
@@ -1,1 +1,12 @@
-wordBank = ['amalgamation','gathering','hilarious','particular','representations','sodiumcholrine','carbohydrate']
+import time
+
+from time import sleep
+
+for i in range(0,7):
+    print(7-i)
+    sleep(1)
+print("Game over!")
+
+
+openPlz = (wordbank.txt,"r")
+readPlz = openPlz.read()
