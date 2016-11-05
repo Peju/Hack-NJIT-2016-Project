8a881bc1f1d3fe9eeb220920b79eeeb62d82cb3d
Peju <pjough@gmail.com>
2016-11-05 16:20:31 -0400

    v6

--- hacknjit2016.py
+++ hacknjit2016.py
@@ -1,11 +1,11 @@
 import time
 import random
 
-openPlz = ('wordbank.txt',"r")
+openPlz = open('wordbank.txt','r')
 readPlz = openPlz.read()
 wordBank = readPlz.split()
 
-stat = true
+stat = True
 strike = 0
 score = 0
 
@@ -14,18 +14,31 @@ def gameMain(wordBank):
     #lst[0] = added points, lst[1] = added strikes
     lst = [0,0]
     start = time.time()
-    while time.time()-start < 7:
-        wordQuiz = wordBank[random.randint(0,len(wordBank))]
-        wordType = input('Type the word '+ wordQuiz)
-    if wordType == wordQuiz:
+    wordQuiz = wordBank[random.randint(0,(len(wordBank)-1))]
+    wordType = input('Type the word '+ wordQuiz + ': ')
+    if wordType == wordQuiz and time.time()-start < 7:
         lst[0] += 1
+    elif time.time()-start >= 7:
+        print('STRIKE! Too Slow!')
+        lst[1] += 1
     else:
+        print('STRIKE! Watch your spelling!')
         lst[1] += 1
     return lst    
+def rsg():
+    print('Ready?')
+    time.sleep(1)
+    print('Set?')
+    time.sleep(1)
+    print('Go!')
+    time.sleep(1)
 
-print("Type the word then press enter!")
 
-while stat == true:
+print("Type the word then press enter in under 7 seconds!")
+time.sleep(3)
+rsg()
+
+while stat == True:
     lst = gameMain(wordBank)
     score += lst[0]
     strike += lst[1]
@@ -35,5 +48,8 @@ while stat == true:
 print('Game Over!')
 print('Your Score: ' + str(score))
 
-
 openPlz.close()
+
+'''
+for i in splitHigh:
+'''
