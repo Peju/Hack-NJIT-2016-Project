316daf95692af50577221f3ead37a7ec96f580c5
jm672 <jm672@njit.edu>
2016-11-05 13:59:01 -0400

    Update hacknjit2016.py

--- hacknjit2016.py
+++ hacknjit2016.py
@@ -1,13 +1,6 @@
 import time
 import random
 
-from time import sleep
-
-for i in range(0,7):
-    print(7-i)
-    sleep(1)
-print("Game over!")
-
 
 openPlz = (wordbank.txt,"r")
 readPlz = openPlz.read()
@@ -20,13 +13,16 @@ strike = 0
 score = 0
 print("Type the word then press enter!")
 while stat == true:
-    wordQuiz = wordBank[random.randint(0,len(wordBank))]
-    wordType = input(wordQuiz)
-    if wordType == wordQuiz:
-        score += 1
-        continue
-    else:
-        strike += 1
+    start = time.time()
+    while time.time()-start < 7:
+        wordQuiz = wordBank[random.randint(0,len(wordBank))]
+        wordType = input(wordQuiz)
+        if wordType == wordQuiz:
+            score += 1
+            continue
+        else:
+            strike += 1
+    strike += 1
     if strike == 3:
-        print('Game Over!')
-        break
+            print('Game Over!')
+            break
